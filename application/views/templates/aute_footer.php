65f611823b4ce15a79d4c9064ee268192c7fcdd6
farizan-il <farizanilham7@gmail.com>
2024-06-13 09:42:12 +0700

    Upload Tugas [all]

---
+++ application/views/templates/aute_footer.php
@@ -0,0 +1,12 @@
+    <!-- Bootstrap core JavaScript-->
+    <script src="http://localhost/pustaka/assets/vendor/jquery/jquery.min.js"></script>
+    <script src="http://localhost/pustaka/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- Core plugin JavaScript-->
+    <script src="http://localhost/pustaka/assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <!-- Custom scripts for all pages-->
+    <script src="http://localhost/pustaka/assets/js/sb-admin-2.min.js"></script>
+    <script>
+        $('.alert-message').alert().delay(3000).slideUp('slow');
+    </script>
+    </body>
+</html>
